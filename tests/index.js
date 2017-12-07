5bec7b3ee3f200fdafefff397388301f5296820e
jacekelgda <jacekelgda@gmail.com>
2017-12-07 17:17:49 +0100

    Two steps almost working --wip #49

--- tests/index.js
+++ tests/index.js
@@ -1,28 +1,32 @@
+require('dotenv').config()
 const { Builder, By, Key, until } = require('selenium-webdriver')
-
 const DEVELOPER_UNLEASHER_AUTHOR = 'developer_unleasher'
 const WAITING_FOR_RESPONSE = 5000
+const I_SEND_CHAT_MESSAGE = 'iSendChatMessage'
 
 let driver = new Builder()
     .forBrowser('safari')
     .build()
 
-driver.get('https://jrl-it-nord.slack.com/messages/D5FTEJ85C/')
+driver.get(process.env.url)
 driver.wait(until.titleIs('Slack'))
 driver.findElement(By.name('email')).sendKeys(process.env.email)
 driver.findElement(By.name('password')).sendKeys(process.env.password, Key.RETURN)
 driver.wait(until.titleIs('developer_unleasher | it-nord Slack'))
-driver.findElement(By.id('msg_input')).sendKeys('hi', Key.RETURN)
-driver.sleep(WAITING_FOR_RESPONSE);
 
 const trimContent = (content) => {
   return content.replace(/[\r\n\t]+/g, '\n').trim()
 }
 
-const compareContent = (content) => {
-  const expectedContent = "Hi! I can see that you have no goals yet. Shal we create one?\nCreate Goal\nNot today ...\nContact Real Unleasher"
-  if (expectedContent != trimContent(content)) {
+const compareContent = (content, expectedContent) => {
+  content = trimContent(content)
+  if (content != expectedContent) {
+    console.log('Provided: ', content)
+    console.log('Expected: ', expectedContent)
+
     throw 'Content doesn\'t match'
+  } else {
+    console.log('Test ok.')
   }
 }
 
@@ -37,7 +41,7 @@ const checkIfBotResponded = () => {
   })
 }
 
-const checkLastMessage = () => {
+const getLastMessageInChat = () => {
   const messageContentScript = "return document.querySelector('#msgs_div > .day_container:last-child > .day_msgs > .message:last-child > .message_content > .message_body');"
   return new Promise((resolve, reject) => {
     driver.executeScript(messageContentScript).then((title) => {
@@ -48,11 +52,44 @@ const checkLastMessage = () => {
   })
 }
 
-const init = async () => {
-  compareContent(await checkLastMessage())
+const compareLastMessageContent = async (expectedContent) => {
+  compareContent(await getLastMessageInChat(), expectedContent)
 }
 
-init()
+const iSendChatMessage = (message) => {
+  return driver.findElement(By.id('msg_input')).sendKeys(message, Key.RETURN)
+}
 
+const iClickInteractiveMessageButton = async(title) => {
+  const messageContentScript = `return document.querySelector("#msgs_div > .day_container:last-child > .day_msgs > .message:last-child > .message_content > .message_body > .attachment_group > .inline_attachment > .column_content button[title='${title}']");`
+  driver.executeScript(messageContentScript).then((el) => {
+    el.click()
+  })
+}
 
+const when = (action) => {
+  return action()
+}
+
+const and = (action) => {
+  return action()
+}
+
+const chatResponseShouldBe = async (expectation) => {
+  driver.sleep(WAITING_FOR_RESPONSE)
+  await compareLastMessageContent(expectation)
+}
+
+const runExpectations = () => {
+  when(() =>
+    iSendChatMessage("Hi!"))
+  .then(() =>
+    chatResponseShouldBe("Hi! I can see that you have no goals yet. Shal we create one?\nCreate Goal\nNot today ...\nContact Real Unleasher"))
+  and(() =>
+    iClickInteractiveMessageButton("Not today ..."))
+  .then(
+    chatResponseShouldBe("Ok, thx!"))
+}
+
+runExpectations()
 driver.quit()
